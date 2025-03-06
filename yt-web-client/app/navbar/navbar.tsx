78640b32ca7000fadf8597a8fa1bec99be517c61
ishaan <ishaanbhalla1998@gmail.com>
2025-03-05 22:35:14 -0800

    feat: init firebase auth - Install firebase as dependency. - Create `firebase.ts` to setup service. - Add `sign-in` button with Google popup functionality.

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,8 +1,26 @@
+"use client";
 import Image from "next/image";
 import styles from "./navbar.module.css";
 import Link from "next/link";
+import SignIn from "./sign-in";
+import { onAuthStateChangeHelper } from "../firebase/firebase";
+import { useState, useEffect } from "react";
+import { User } from "firebase/auth";
 
 export default function Navbar() {
+  // Init user state
+  const [user, setUser] = useState<User | null>(null);
+
+  // This hook will render some JS just a single time once this function actually loads
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangeHelper((user) => {
+      setUser(user);
+    });
+
+    // Cleanup
+    return () => unsubscribe();
+  });
+
   return (
     <nav className={styles.nav}>
       <Link href={"/"}>
@@ -13,6 +31,7 @@ export default function Navbar() {
           alt="YouTube Logo"
         />
       </Link>
+      <SignIn user={user} />
     </nav>
   );
 }
